62e323fa751962e77226b9d601ea8ba8d037c00e
xavier skip <xavierskip@gmail.com>
2020-10-12 16:32:00 +0800

    cmd log error

--- do.bat
+++ do.bat
@@ -2,6 +2,6 @@
 set base_dir=%~dp0  
 %base_dir:~0,2%
 pushd %base_dir%
-python run.py >>  data/log/%date:~0,4%-%date:~5,2%.log  
-echo # %time%  >> data/log/%date:~0,4%-%date:~5,2%.log
+python run.py >>  data/log/%date:~0,4%-%date:~5,2%.log 2>&1
+echo #%date:~5,2%-%time%  >> data/log/%date:~0,4%-%date:~5,2%.log
 popd
