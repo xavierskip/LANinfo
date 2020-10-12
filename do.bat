92e98a8f540fa578fe61640a22e36ebff42a1a1d
xavier skip <xavierskip@gmail.com>
2020-10-12 16:47:54 +0800

    cmd data

--- do.bat
+++ do.bat
@@ -3,5 +3,5 @@ set base_dir=%~dp0
 %base_dir:~0,2%
 pushd %base_dir%
 python run.py >>  data/log/%date:~0,4%-%date:~5,2%.log 2>&1
-echo #%date:~8,2%*%time%  >> data/log/%date:~0,4%-%date:~5,2%.log
+echo #%date:~8,2%-%time%  >> data/log/%date:~0,4%-%date:~5,2%.log
 popd
